b69462bf21e43e500595e9e45b16bc86d71c6c3b
Rukundo Aime <rukundoaime78@gmail.com>
2024-04-17 20:28:38 +0200

    add product detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full', 
+    redirectTo: 'products', // Redirect to '/products' path
+  },
+  {
+    path: 'products',
     component: HomeComponent,
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailComponent,
   }
 ];
 
--- src/app/services/auth-modal.service.ts
+++ src/app/services/auth-modal.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class AuthModalService {
-  public visible = true;
+  public visible = false;
   constructor() {}
 
   closeModal() {
